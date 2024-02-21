5e7d302eb93c170fbec885646f3df17305afbb46
xovel <xovel@vip.qq.com>
2024-02-21 18:28:34 +0800

    [feature] use puppeteer to load pgn

---
+++ list2.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const puppeteer = require('puppeteer');
+
+const path = require('path');
+const fs = require('fs');
+
+const wFile = require('./util/wFile');
+
+const pgn = require('./pgn.json');
+
+const list = pgn.list;
+
+if (!fs.existsSync('pgn')) {
+  fs.mkdirSync('pgn');
+}
+
+async function fetchList() {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+
+  const page = await browser.newPage();
+
+  for (item of list) {
+
+    const curFile = path.join(__dirname, 'pgn', `${item.date}-${item.id}.pgn`);
+
+    if (fs.existsSync(curFile)) {
+      continue;
+    }
+
+    const p = `https://www.chessgames.com/perl/nph-chesspgn?text=1&gid=${item.id}`;
+    await page.goto(p);
+
+    const text = await page.evaluate(() => {
+      return document.querySelector('html').innerText;
+    });
+
+    await wFile(curFile, text);
+  }
+
+  await browser.close();
+};
+
+fetchList();
