8c7c6644c56a8ccc209b146afdee68aeed9fa06e
xovel <xovel@vip.qq.com>
2026-04-23 14:04:44 +0800

    [feature] sleep while fetch content has been limited

--- list2.js
+++ list2.js
@@ -9,10 +9,25 @@ const wFile = require('./util/wFile');
 
 const pgn = require('./pgn.json');
 
+const sleep = t => new Promise(r => {
+  console.log('sleep...', t);
+  setTimeout(() => {
+    r();
+  }, t);
+});
+
+function rnd(a, b) {
+  return a + Math.floor(Math.random() * (b - a))
+}
+
 const list = pgn.list;
 
 if (!fs.existsSync('pgn')) {
   fs.mkdirSync('pgn');
+}
+
+if (!fs.existsSync('temp')) {
+  fs.mkdirSync('temp');
 }
 
 async function fetchList() {
@@ -29,23 +44,77 @@ async function fetchList() {
     page = await browser.newPage();
   }
 
-  for (const item of list) {
+  let needGotoPage = true;
 
-    const curFile = path.join(__dirname, 'pgn', `${item.date}-${item.id}.pgn`);
+  let wait = 5000;
+  let n = 0;
+  let retry = false;
 
-    if (fs.existsSync(curFile)) {
-      continue;
+  async function loop() {
+
+    for (const item of list) {
+
+      const curFile = path.join(__dirname, 'pgn', `${item.date}-${item.id}.pgn`);
+
+      if (fs.existsSync(curFile)) {
+        const fileContent = fs.readFileSync(curFile).toString();
+        if (fileContent && fileContent[0] === '[') {
+          console.log('file exist, ingored...', curFile);
+          continue;
+        }
+        console.log('File exists, but content error', fileContent);
+        console.log('deleting...', curFile);
+        fs.unlinkSync(curFile);
+        console.log('Ignored');
+        continue;
+      }
+
+      const p = `https://www.chessgames.com/perl/nph-chesspgn?text=1&gid=${item.id}`;
+
+      if (needGotoPage) {
+        await page.goto(p);
+        needGotoPage = false;
+      }
+
+      await sleep(wait + rnd(0, 800));
+      n++;
+      if (n % 5 === 0) {
+        wait += rnd(0, 2000);
+      }
+
+      // const text = await page.evaluate(() => {
+      //   return document.querySelector('html').innerText;
+      // });
+
+      console.log('fetch...', p);
+
+      const text = await page.evaluate(async function (url) {
+        return await fetch(url).then(res => res.text());
+      }, p);
+
+      if (text[0] !== '[') {
+        console.log('file error', text);
+        const tempFilePath = path.join(__dirname, 'temp', `${item.date}-${item.id}-${Date.now()}.html`);
+        await wFile(tempFilePath, text);
+        retry = true;
+        break;
+      }
+
+      await wFile(curFile, text);
     }
 
-    const p = `https://www.chessgames.com/perl/nph-chesspgn?text=1&gid=${item.id}`;
-    await page.goto(p);
+    if (retry) {
+      retry = false;
+      wait = 5000;
+      n = 0;
+      console.log('retry...');
+      await sleep(120000);
+      await loop();
+    }
 
-    const text = await page.evaluate(() => {
-      return document.querySelector('html').innerText;
-    });
-
-    await wFile(curFile, text);
   }
+
+  await loop();
 
   await browser.close();
 };
